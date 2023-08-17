bc9991a73365a598015d66d0155bf13b5e16f347
PedroABarros <pedrokii.a.b@gmail.com>
2023-08-16 22:18:28 -0300

    Atualização dados

--- Aula2/dicionario.py
+++ Aula2/dicionario.py
@@ -11,3 +11,11 @@ dicionario2["Telefone"] = "32 3287-4373"
 
 print(dicionario)
 print(dicionario2)
+
+print("-=" * 15)
+for k, v in dicionario.items():
+    print(k, v)
+
+print("-=" * 15)
+for k, v in dicionario2.items():
+    print(k, v)
