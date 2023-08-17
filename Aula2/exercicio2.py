c66d3195411b22ed235d3b4b0a765896390c2bf8
PedroABarros <pedrokii.a.b@gmail.com>
2023-08-16 22:02:08 -0300

    Exercício de dicionario

---
+++ Aula2/exercicio2.py
@@ -0,0 +1,13 @@
+dicionario = {}
+dicionario2 = {}
+
+dicionario["nome"] = "Pedro"
+dicionario["idade"] = "20"
+dicionario["Telefone"] = "32 9087-6473"
+
+dicionario2["nome"] = "Joao"
+dicionario2["idade"] = "23"
+dicionario2["Telefone"] = "32 3287-4373"
+
+print(dicionario)
+print(dicionario2)
